7bb1643d84a6d80531855a5d0fb012f21bc48c3d
Lenovo <vivekmane9860@gmail.com>
2026-03-14 18:57:38 +0530

    Update application entry points for Eureka, Orchestrator, and Worker services

--- orchestrator-service/src/main/java/org/workflow/orchestratorservice/OrchestratorServiceApplication.java
+++ orchestrator-service/src/main/java/org/workflow/orchestratorservice/OrchestratorServiceApplication.java
@@ -2,8 +2,14 @@ package org.workflow.orchestratorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"org.workflow.coremodels.model"})
+@EnableJpaRepositories(basePackages = {"org.workflow.coremodels.repository"})
+@EnableScheduling
 public class OrchestratorServiceApplication {
 
 	public static void main(String[] args) {
--- worker-service/src/main/java/org/workflow/workerservice/WorkerServiceApplication.java
+++ worker-service/src/main/java/org/workflow/workerservice/WorkerServiceApplication.java
@@ -2,8 +2,12 @@ package org.workflow.workerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"org.workflow.coremodels.model"})
+@EnableJpaRepositories(basePackages = {"org.workflow.coremodels.repository"})
 public class WorkerServiceApplication {
 
 	public static void main(String[] args) {
